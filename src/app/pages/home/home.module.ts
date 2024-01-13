9650a50d9f118a649d7bae2cb1e7e815db72a065
AnwarMEQOR <itsanwarmeqor@gmail.com>
2024-01-13 17:30:45 +0100

    updated structure

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common'
 
 import { HomeRoutingModule } from './home-routing.module'
 import { HomeComponent } from './home.component'
-import { HeroComponent } from './components/hero/hero.component'
 import { TypingEffectDirective } from '../../directives/typing-effect.directive'
 
 @NgModule({
-	declarations: [HomeComponent, HeroComponent, TypingEffectDirective],
+	declarations: [HomeComponent, TypingEffectDirective],
 	imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
