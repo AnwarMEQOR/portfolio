df1c025980a45c93180b58b2b189ed79dee418f9
AnwarMEQOR <itsanwarmeqor@gmail.com>
2024-01-13 19:13:44 +0100

    added interests

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+	hobbies: { name: string; icon: string }[] = [
+		{ name: 'Classical Music', icon: 'music-note-beamed' },
+		{ name: 'Cooking', icon: 'basket3-fill' },
+		{ name: 'Anime / Manga', icon: 'tv' },
+		{ name: 'Gaming', icon: 'controller' },
+		{ name: 'Photography', icon: 'camera' },
+		{ name: 'Cultures', icon: 'backpack2' },
+		{ name: 'Volunteering', icon: 'person-raised-hand' },
+		{ name: 'Fitness', icon: 'heart-pulse' },
+	]
 }
