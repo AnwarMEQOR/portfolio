e2324e64d6d4fe309a781b1aed9bb8f03b9a7064
AnwarMEQOR <itsanwarmeqor@gmail.com>
2024-01-18 16:53:09 +0100

    added about and services

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -22,6 +22,38 @@ export class HomeComponent implements OnInit {
 		{ name: 'Articles Published', icon: 'file-earmark-richtext', finalCount: 3, count: 0 },
 		{ name: 'Years in the Field', icon: 'calendar2-range', finalCount: 4, count: 0 },
 	]
+	services: { title: string; icon: string; text: string }[] = [
+		{
+			title: 'Web Application Development',
+			icon: 'code-slash',
+			text: 'Crafting dynamic, responsive, and intuitive web applications using modern technologies and frameworks to meet your unique business needs.',
+		},
+		{
+			title: 'API Development & Integration',
+			icon: 'plug-fill',
+			text: 'Designing robust and scalable APIs, and integrating third-party services to enhance the functionality and performance of your applications.',
+		},
+		{
+			title: 'Database Design & Management',
+			icon: 'server',
+			text: 'Offering comprehensive database solutions, including design, development, optimization, and management to ensure data integrity and performance.',
+		},
+		{
+			title: 'Cloud Services & Deployment',
+			icon: 'cloud-arrow-up-fill',
+			text: 'Leveraging cloud technologies to deploy, manage, and scale applications efficiently, ensuring high availability and cost-effectiveness.',
+		},
+		{
+			title: 'SEO Optimization & Analysis',
+			icon: 'graph-up-arrow',
+			text: "Enhancing your website's visibility and ranking on search engines through SEO optimization and analysis. Implementing best practices to drive organic traffic.",
+		},
+		{
+			title: 'Cybersecurity & Data Protection',
+			icon: 'shield-lock-fill',
+			text: 'Prioritizing the security of your digital assets with comprehensive cybersecurity solutions. Implementing robust security protocols and ensuring data protection',
+		},
+	]
 	@ViewChild('counter') counterElement: ElementRef | undefined
 	hasAnimatedCounter = false // To ensure the counter animation only runs once
 	ngOnInit() {
