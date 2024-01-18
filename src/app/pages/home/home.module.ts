2aca601ef81891fd96cf37c5bcd54a7af9f215e5
AnwarMEQOR <itsanwarmeqor@gmail.com>
2024-01-18 23:09:50 +0100

    updated seo and accessibility

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
+import { CommonModule, NgOptimizedImage } from '@angular/common'
 
 import { HomeRoutingModule } from './home-routing.module'
 import { HomeComponent } from './home.component'
@@ -7,6 +7,6 @@ import { TypingEffectDirective } from '../../directives/typing-effect.directive'
 
 @NgModule({
 	declarations: [HomeComponent, TypingEffectDirective],
-	imports: [CommonModule, HomeRoutingModule],
+	imports: [CommonModule, HomeRoutingModule, NgOptimizedImage],
 })
 export class HomeModule {}
